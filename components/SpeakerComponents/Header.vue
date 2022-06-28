8cc0b2973f0d45bfd33a71c260e4e828dd20f81a
Kai Reichart <kai@reichart.dev>
2022-06-28 21:34:13 +0100

    header component

--- components/SpeakerComponents/Header.vue
+++ components/SpeakerComponents/Header.vue
@@ -1,30 +1,40 @@
 <template>
-  <div class="header">
-    <Nav />
-    <div class="spacer" />
-    <div class="SpeakerInfo w-100-vw">
-      <Container>
-        <div class="grid grid-cols-1 md:grid-cols-3">
-          <div class="image-border bg-red-400 rounded-full p-0 m-0 mx-auto">
-            <img :src="computedImg" alt="" class="rounded-full object-cover p-1">
+  <div class="header relative text-white">
+    <RepeatingImage
+      :image="require('~/assets/images/hero1.jpg')"
+      :mobile-image="require('~/assets/images/hero1_mobile.jpg')"
+      :repeat-left="require('~/assets/images/hero1_left.jpg')"
+      :repeat-right="require('~/assets/images/hero1_right.jpg')"
+    >
+      <div class="spacer" />
+      <div class="SpeakerInfo">
+        <Container>
+          <div class="grid grid-cols-1 md:grid-cols-3">
+            <div class="image-border bg-red-400 rounded-full p-0 m-0 mx-auto">
+              <img :src="computedImg" alt="" class="rounded-full object-cover p-1">
+            </div>
+            <div class="my-2 md:my-5"/>
+            <div class="mx-auto text-xl">
+              <ul>
+                <li v-if="speaker.twitter">
+                  <a :href="'https://twitter.com/' + speaker.twitter" target="_blank"><i class="fab fa-twitter-square" /> {{ speaker.twitter }}</a>
+                </li>
+                <li v-if="speaker.github">
+                  <a :href="'https://github.com/' + speaker.github" target="_blank"><i class="fab fa-github-square" /> {{ speaker.github }}</a>
+                </li>
+                <li v-if="speaker.linkedin">
+                  <a :href="'https://linkedin.com/in/' + speaker.linkedin" target="_blank"><i class="fab fa-linkedin" /> {{ speaker.linkedin }}</a>
+                </li>
+              </ul>
+            </div>
           </div>
-          <div class="my-2 md:my-5"/>
-          <div class="mx-auto text-xl">
-            <ul>
-              <li v-if="speaker.twitter">
-                <a :href="'https://twitter.com/' + speaker.twitter" target="_blank"><i class="fab fa-twitter-square" /> {{ speaker.twitter }}</a>
-              </li>
-              <li v-if="speaker.github">
-                <a :href="'https://github.com/' + speaker.github" target="_blank"><i class="fab fa-github-square" /> {{ speaker.github }}</a>
-              </li>
-              <li v-if="speaker.linkedin">
-                <a :href="'https://linkedin.com/in/' + speaker.linkedin" target="_blank"><i class="fab fa-linkedin" /> {{ speaker.linkedin }}</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </Container>
-    </div>
+        </Container>
+      </div>
+    </RepeatingImage>
+    <SlimContainer class="flex items-center justify-end">
+      <Nav />
+    </SlimContainer>
+
   </div>
 </template>
 
@@ -48,15 +58,15 @@ export default {
 </script>
 
 <style scoped>
-.header{
-  color: white;
-  position: relative;
-  width: 100%;
-  background-image: url("~/assets/images/hero1.jpg");
-  background-size: cover;
-  background-position: top center;
-  min-height: fit-content;
-}
+/*.header{*/
+/*  color: white;*/
+/*  position: relative;*/
+/*  width: 100%;*/
+/*  background-image: url("~/assets/images/hero1.jpg");*/
+/*  background-size: cover;*/
+/*  background-position: top center;*/
+/*  min-height: fit-content;*/
+/*}*/
 
 .image-border{
   background: linear-gradient(to right, #ffcc00, #e60000);
